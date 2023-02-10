77afcc2062b207613d8ff68f508cc6752d0444b7
Mohamed Ramzy <mohamedramzy2095@gmail.com>
2023-02-10 05:18:48 +0200

    first Commit

---
+++ JWT_WebAPI/Models/RevokeToken.cs
@@ -0,0 +1,8 @@
+﻿namespace JWT_WebAPI.Models
+{
+    public class RevokeToken
+    {
+        //why not required?? you can catch the token from the body of the request or from cookie
+        public string? Token { get; set; }
+    }
+}
---
+++ JWT_WebAPI/Services/IAuthService.cs
@@ -0,0 +1,16 @@
+﻿using JWT_WebAPI.Models;
+
+namespace JWT_WebAPI.Services
+{
+    public interface IAuthService
+    {
+        Task<AuthModel> Registerasync(RegisterModel model);
+        Task<AuthModel> GetTokenAsync(TokenRequestModel model);
+
+        Task<string> AddRoleAsync(AddRoleModel model);
+        Task<AuthModel> RefreshTokenAsync(string token);
+
+        Task<bool> RevokeTokenAsync(string token);
+
+    }
+}
